deee544b2bbed10c9f60f67f98bac43d0336b3d9
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-02-22 10:11:50 -0300

    updating values in the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,58 +2,34 @@ package application;
 
 import db.DB;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
+        PreparedStatement statement = null;
         try {
             connection = DB.getConnection();
-
-            preparedStatement = connection.prepareStatement(
-                    "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                            + "VALUES "
-                            + "(?, ?, ? , ?, ?)"
-                    ,
-                    Statement.RETURN_GENERATED_KEYS
+            statement = connection.prepareStatement(
+                    "UPDATE seller "
+                            + "SET BaseSalary = BaseSalary + ?"
+                            + "WHERE "
+                            + "(DepartmentId = ?)"
             );
 
-            preparedStatement.setString(1, "Carl Purple");
-            preparedStatement.setString(2, "carl@gmail.com");
-            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            preparedStatement.setDouble(4, 3000.0);
-            preparedStatement.setInt(5, 4);
+            statement.setDouble(1, 200.0);
+            statement.setInt(2, 2);
 
-            /* essa query ira retornar mais de uma key, pois foi adicionado mais de 1 valor no bando de dados
-            preparedStatement = connection.prepareStatement(
-                    "INSERT INTO department (Name) VALUES ('D1'), ('D2')"
-                    ,
-                    Statement.RETURN_GENERATED_KEYS
-            );*/
+            int rowsAffected = statement.executeUpdate();
 
-            int rowsAffected = preparedStatement.executeUpdate();
-
-            if (rowsAffected > 0) {
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-                while (resultSet.next()) {
-                    int id = resultSet.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            } else {
-                System.out.println("Now rown affected");
-            }
-
-        } catch (ParseException | SQLException e) {
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            DB.closeStatement(preparedStatement);
             DB.closeConnection();
+            DB.closeStatement(statement);
         }
     }
 }
