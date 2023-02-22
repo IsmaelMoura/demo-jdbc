e11e847d56f49ca91e56be4d81328110f5046ce7
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-02-22 09:44:39 -0300

    getting information from the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,30 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-        DB.closeConnection();
+        try {
+            connection = DB.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(statement);
+            DB.closeResultSet(resultSet);
+            DB.closeConnection();
+        }
     }
 }
